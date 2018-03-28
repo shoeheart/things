e2c5a91e93e11b5d27d822eccc692dfc8bcc7984
shoeheart <jason@shoeheart.com>
2018-03-28 01:12:37 -0500

    first working auth0 permissions with rudimentary enforcement in animals and lookups

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -5,4 +5,33 @@ module SessionHelper
 
     state
   end
+
+  PERMISSIONS_NAMESPACE = "https://codebarn.com/claims/permissions"
+  def get_permissions
+    session[:userinfo][:extra][:raw_info][PERMISSIONS_NAMESPACE] if (
+      session[:userinfo] &&
+      session[:userinfo][:extra] &&
+      session[:userinfo][:extra][:raw_info]
+    )
+  end
+
+  def write_animals?
+    get_permissions.include?( "read-write:animals" )
+  end
+
+  def read_animals?
+    get_permissions.include?( "read:animals" ) || write_animals?
+  end
+
+  def write_lookups?
+    get_permissions.include?( "read-write:lookups" )
+  end
+
+  def read_lookups?
+    get_permissions.include?( "read:lookups" ) || write_lookups?
+  end
+
+  def say_hello
+    "Hello, world"
+  end
 end
--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,6 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if ENV['AUTH0_AUDIENCE'].blank?
-    audience = URI::HTTPS.build(host: ENV['AUTH0_DOMAIN'], path: '/userinfo').to_s
+    audience =
+      URI::HTTPS.build(
+        host: ENV['AUTH0_DOMAIN'],
+        path: '/userinfo'
+      ).to_s
   else
     audience = ENV['AUTH0_AUDIENCE']
   end
@@ -12,8 +16,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     ENV['AUTH0_DOMAIN'],
     callback_path: '/auth/auth0/callback',
     authorize_params: {
-        scope: 'openid profile',
-        audience: audience
+      scope: 'openid email profile groups permissions roles',
+      audience: audience
     }
   )
 end
