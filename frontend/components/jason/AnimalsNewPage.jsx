b4c68ecb8a8ad50ef93f09a12ce9f33880e663a0
shoeheart <jason@codebarn.com>
2018-07-24 23:01:56 -0500

    enable server side rendering for AnimalsIndexPage and AnimalsNewPage

--- frontend/components/jason/AnimalsNewPage.jsx
+++ frontend/components/jason/AnimalsNewPage.jsx
@@ -5,16 +5,13 @@ import AnimalForm from "components/jason/AnimalForm.jsx";
 class AnimalsNewPage extends React.Component {
   /* eslint class-methods-use-this: "error" */
   render() {
-    const authenticityToken = document.head.querySelector(
-      'meta[name="csrf-token"]'
-    ).content;
     return (
       <div>
         <Menu />
         <AnimalForm
-          authenticity_token={authenticityToken}
           animal={this.props.animal}
           species={this.props.species}
+          authenticity_token={this.props.authenticity_token}
         />
       </div>
     );
