6e3f5f4b6d3a62e6ea8e9065bf0992caac22c990
shoeheart <jason@codebarn.com>
2018-07-27 20:37:17 -0500

    serve static assets from puma on production since we don't have separate cdn or assets server for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+ frozen_string_literal: true
 
 Rails.application.configure do
   # Verifies that versions and hashed value of the package contents in the project's package.json
@@ -24,7 +24,8 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  # config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.enabled = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
